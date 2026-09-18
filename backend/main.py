fb3a6b8b3fc666cf43c63e6802da37ed525d1325
Prem Prakash Pandey <96074642+Prempandeyy@users.noreply.github.com>
2026-09-18 18:11:47 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,9 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://politicaltea-fy2f0rke8-prempandey812743gmailcoms-projects.vercel.app","https://politicaltea.vercel.app"],
+    allow_origins=["http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://politicaltea-fy2f0rke8-prempandey812743gmailcoms-projects.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
